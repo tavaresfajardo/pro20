69e4c836cc6c64d6c9fd1ca08a3bae26c2ea9f0b
Pedro Miguel Tavares Fajardo <tavaresfajardo@ua.pt>
2018-12-14 00:31:23 +0000

    rasginha

--- edc_tp2/guns/views.py
+++ edc_tp2/guns/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpRequest,HttpResponse,Http404
-
+import requests
+url = 'https://query.wikidata.org/sparql'
 
 def home(request):
     
+    query = 'SELECT ?name ?nameLabel ?image WHERE { ?name wdt:P31 wd:Q12796. SERVICE wikibase:label { bd:serviceParam wikibase:language "en". } OPTIONAL { ?name wdt:P18 ?image. } }'
+    r = requests.get(url, params = {'format': 'json', 'query': query})
+    data = r.json()
+    data = [{   'name': v['name']['value'], 'nameLabel': v['nameLabel']['value'] } for v in data['results']['bindings'] ]   
+    print(data)
 
 
     tparams = {
