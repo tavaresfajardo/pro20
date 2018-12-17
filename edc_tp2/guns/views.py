c9a2ccb043b4cbfdfd9e8f5b8aef6d0f3edc71ef
Joao Paulo <joaoprcf@ua.pt>
2018-12-17 23:53:35 +0000

    Query to top guns, report started, preprocess to create database, django integration

--- edc_tp2/guns/views.py
+++ edc_tp2/guns/views.py
@@ -1,19 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpRequest,HttpResponse,Http404
 import requests
+import json
+from s4api.graphdb_api import GraphDBApi
+from s4api.swagger import ApiClient
 url = 'https://query.wikidata.org/sparql'
+endpoint = "http://localhost:7200"
 
 def home(request):
+
+
     
-    query = 'SELECT ?name ?nameLabel ?image WHERE { ?name wdt:P31 wd:Q12796. SERVICE wikibase:label { bd:serviceParam wikibase:language "en". } OPTIONAL { ?name wdt:P18 ?image. } }'
-    r = requests.get(url, params = {'format': 'json', 'query': query})
-    data = r.json()
-    data = [{   'name': v['name']['value'], 'nameLabel': v['nameLabel']['value'] } for v in data['results']['bindings'] ]   
-    print(data)
+    #query to retrieve the 9 guns with the most stock in the store with their correspondent image
+    query = """         
+        PREFIX prop: <http://www.wikidata.org/wiki/Property/>
+        PREFIX entity: <http://www.wikidata.org/entity/>
+        select DISTINCT ?item ?image ?name ?count  where { 
+            ?item prop:P18 ?image .
+            ?item prop:P2561 ?name .
+            ?item prop:P1114 ?count .
+        }
 
-
-    tparams = {
+        ORDER BY DESC(?count)
         
+        limit 9
+    """
+
+   
+    bindings = executeQuery(query)
+    items = [{'name':i['name']['value'], 'image':i['image']['value'], 'count':i['count']['value'] } for i in bindings ]
+
+    # objective: [['a1','a2','a3'],['b1','b2','b3'],['c1','c2','c3']]
+
+    gunlist = [items[i:i + 3] for i in range(0, len(items), 3)]
+    tparams = {
+        'gunlist': gunlist
     }
     return render(request, 'index.html', tparams)
 
@@ -27,3 +48,15 @@ def weapontype(request):
         
     }
     return render(request, 'weapontype.html', tparams)
+
+
+
+def executeQuery(query):   #function to avoid repeating code
+    repo_name = "Guns"
+    client = ApiClient(endpoint=endpoint)
+    accessor = GraphDBApi(client)
+    payload_query = {"query": query}
+    res = accessor.sparql_select(body=payload_query,repo_name=repo_name)
+    res = json.loads(res)
+    bindings = res['results']['bindings']
+    return bindings
---
+++ edc_tp2/preprocess/CreateDatabase.py
@@ -0,0 +1,47 @@
+import json
+import requests
+import random
+url = 'https://query.wikidata.org/sparql'
+wikiurl_entity = '<http://www.wikidata.org/entity/'
+wikiurl_prop = '<http://www.wikidata.org/wiki/Property/'
+
+
+newDB = []
+newDB +=  [(wikiurl_prop+'P2561>', wikiurl_prop+'P2561>' , '"name"')]
+newDB +=  [(wikiurl_prop+'P1114>', wikiurl_prop+'P2561>' , '"count"')]
+newDB +=  [(wikiurl_prop+'P18>', wikiurl_prop+'P2561>' , '"image"')]
+queue = [name]
+csv = []
+d = {}
+while queue:
+item, *queue = queue
+
+queue = []
+query =("SELECT ?name ?nameLabel ?name2 ?name2Label ?image WHERE {"
+       "?name wdt:P31 wd:Q12796."
+       "?name2 wdt:P279* ?name."
+       "?name2 wdt:P18 ?image ."
+       'SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }'
+       "OPTIONAL {  }"
+       "}")
+
+print(query)
+r = requests.get(url, params = {'format': 'json', 'query': query})
+data = r.json()
+
+R = []
+#print('data',data)
+R=[{   'name': '<'+v['name2']['value']+'>' , 'nameLabel': '"'+v['name2Label']['value']+'"' , 'image' : '"'+v['image']['value']+'"' }for v in data['results']['bindings'] if v['name2Label']['value'] not in v['name2']['value'] ]
+
+newR = [(i,random.randint(1,10)) for i in random.sample(R, 60)]
+for i in newR:
+       newDB +=  [(i[0]['name'],wikiurl_prop+'P2561>', i[0]['nameLabel'])]
+       newDB +=  [(i[0]['name'],wikiurl_prop+'P1114>',i[1])]
+       newDB +=  [(i[0]['name'],wikiurl_prop+'P18>',i[0]['image'])]
+       print([(i[0]['name'],i[1]) for i in newR])
+
+
+writeData = '\n'.join([' '.join([str(j) for j in list(i)]) + ' .' for i in newDB])
+with open('storage.nt','w') as f:
+       print('OK')
+       f.write(writeData)
